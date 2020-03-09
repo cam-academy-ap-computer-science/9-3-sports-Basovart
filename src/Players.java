556a333f1fd4629d55f9892905e9cdf3c9aff968
basovart000 <basovart000@CAM-LAB-23.bgsd.k12.wa.us>
2020-03-09 07:51:08 -0700

    03.09.2020-Morning

---
+++ src/Players.java
@@ -0,0 +1,14 @@
+
+public class Players {
+	private int age;
+	private String name;
+	private int salary;
+	private String nationality;
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
